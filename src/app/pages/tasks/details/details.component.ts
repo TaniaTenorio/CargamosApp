3c6d5125563a0130f566464cb03157c3ae90e551
TaniaTenorio <taniatenorio@hotmail.com>
2021-05-06 23:05:03 -0500

    use mockData and pass it to details screen

--- src/app/pages/tasks/details/details.component.ts
+++ src/app/pages/tasks/details/details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
+import { Task } from 'src/app/shared/models/task.interface';
 
 @Component({
   selector: 'app-details',
@@ -6,10 +8,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./details.component.scss']
 })
 export class DetailsComponent implements OnInit {
+  
+  task: Task;
+  navigationExtras: NavigationExtras = {
+    state: {
+      value: null
+    }
+  }
 
-  constructor() { }
+  constructor(private router: Router) {
+    const navigation = this.router.getCurrentNavigation();
+    this.task = navigation?.extras?.state?.value;
+  }
 
   ngOnInit(): void {
+    if (typeof this.task === 'undefined') {
+      this.router.navigate(['list']);
+    }
+  }
+
+  onGoToEdit(): void{
+    this.navigationExtras.state.value = this.task;
+    this.router.navigate(['edit'], this.navigationExtras)
+  }
+
+  onGoBackToList():void{
+    this.router.navigate(['list'])
+  }
+
+  onDelete():void{
+    alert('Delete')
+  }
+
+  onFinish(): void{
+    alert('Finished')
   }
 
 }
--- src/app/pages/tasks/list/list.component.ts
+++ src/app/pages/tasks/list/list.component.ts
@@ -8,6 +8,29 @@ import { NavigationExtras, Router } from '@angular/router';
 })
 export class ListComponent implements OnInit {
 
+  mockData = [
+    {
+      name: 'Recolección en Lomas Verdes',
+      description: 'Recolecta paquete con dimensiones 50x50, 1kg a nombre de Luis V.',
+      duration: '60:00',
+    },
+    {
+      name: 'Entrega en Condesa',
+      description: 'Entregar paquete a nombre de Sara J. Querétaro #201-1',
+      duration: '30:00',
+    },
+    {
+      name: 'Recolección en YEMA',
+      description: 'Recolecta paquetería 15pzas ch, 2pzas md antes de las 14:30',
+      duration: '90:00',
+    },
+    {
+      name: 'Recolección y entrega Canasta Rosa',
+      description: 'Recolecta paquete con dimensiones 50x50, a nombre de Tomás M. Entrega en Zacatecas 123-A3',
+      duration: '60:00',
+    },
+  ]
+
   navigationExtras: NavigationExtras = {
     state: {
       value: null
