f573e8c763e962af7d013718d113132483e2bb5d
TaniaTenorio <taniatenorio@hotmail.com>
2021-05-07 23:15:28 -0500

    create login form

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+
+  constructor(private authSvc: AuthService, private fb: FormBuilder, private router: Router) {
+    this.initForm();
+  }
 
   ngOnInit(): void {
+  }
+
+  async onLogin() {
+    const { email, password } = this.loginForm.value;
+    try {
+      const user = this.authSvc.login(email, password)
+      if(user) {
+        this.router.navigate(['home'])
+      }
+    } catch(err) {
+      console.log(err);
+    }
+  }
+
+  private initForm():void {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required]]
+    })
   }
 
 }
