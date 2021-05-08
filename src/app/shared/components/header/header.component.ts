8b0aade5a28a2dc9c4d98549b3586e838d97c2f7
TaniaTenorio <taniatenorio@hotmail.com>
2021-05-07 23:33:46 -0500

    logout functionality and show email if user logged

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  public user$: Observable<any> = this.authSvc.afAuth.user;
+
+  constructor(private authSvc: AuthService, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  async onLogout() {
+    try {
+      await this.authSvc.logout();
+      this.router.navigate(['/login'])
+    } catch(err) {
+      console.log(err);
+    }
   }
 
 }
