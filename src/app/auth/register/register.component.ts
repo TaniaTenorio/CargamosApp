ae741848ab7af9988c25650130c814b3607390a1
TaniaTenorio <taniatenorio@hotmail.com>
2021-05-07 23:03:11 -0500

    create registration form

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
+
+  constructor(private authSvc: AuthService, private fb: FormBuilder, private router: Router) {
+    this.initForm()
+  }
 
   ngOnInit(): void {
+  }
+
+  async onRegister() {
+    const {email, password} = this.registerForm.value;
+    try {
+      const user = await this.authSvc.register(email, password);
+      if (user) {
+        this.router.navigate(['home'])
+      }
+    } catch(err) {
+      console.log(err);
+    }
+  }
+
+  private initForm():void {
+    this.registerForm = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    })
   }
 
 }
